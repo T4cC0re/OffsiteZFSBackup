6437e2bd501346db3939d4050174e049e9cde765
Hendrik Meyer <github@t4cc0.re>
2018-05-26 00:27:17 +0200

    [Snapshot] add SnapshotWithSize

--- Common/Snapshot.go
+++ Common/Snapshot.go
@@ -4,3 +4,10 @@ type Snapshot struct {
 	Uuid     string
 	Filename string
 }
+
+type SnapshotWithSize struct {
+	Uuid         string
+	Filename     string
+	DownloadSize uint64
+	DiskSize     uint64
+}
