58ef3ce58d576433e342078f248689fc268a5efe
Hendrik 'T4cC0re' Meyer <github@t4cc0.re>
2018-06-25 01:20:14 +0200

    Better build script. also pushed to mirror

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-readonly date="$(date --utc +%Y.%m.%d~%H.%M)"
+readonly date="$(date --utc +%Y.%m.%d~%H.%M.%S)"
 readonly tmpdir="$(mktemp -d)"
 
 #go build 2>&1 | grep package | cut -d'"' -f2 | xargs go get -u
@@ -12,6 +12,10 @@ chmod +rx ${tmpdir}
 chmod +s "${tmpdir}/OffsiteZFSBackup"
 fpm -n offsite-zfs-backup -v $date -s dir -t pacman -C "${tmpdir}" .=/opt/ozb
 fpm -n offsite-zfs-backup -v $date -s dir -t deb -C "${tmpdir}" .=/opt/ozb
+scp *.deb *.pkg.tar.xz mirror.t4cc0.re:/vault/packages
+rm *.deb *.pkg.tar.xz || true
 if [ -n "${tmpdir}" ]; then
-  rm -f "${tmpdir}"
+  rm -r "${tmpdir}"
 fi
+
+cd -
