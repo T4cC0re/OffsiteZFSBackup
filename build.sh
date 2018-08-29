349fcfffd13d4ed95d5431803b6b66997e6a8534
Hendrik Meyer <github@t4cc0.re>
2018-08-29 22:38:33 +0200

    Unify build script

--- build.sh
+++ build.sh
@@ -1,21 +1,74 @@
 #!/usr/bin/env bash
 
+readonly date="$(date --utc +%Y.%m.%d~%H.%M.%S)"
+
+ToolExists() {
+  if ! hash $1 &>/dev/null; then
+    echo $1 is not installed
+    exit 1
+  fi
+}
+
+pack () {
+  upx --best --ultra-brute --all-methods --overlay=strip $*
+}
+
+compile () {
+  go build --ldflags="-s -w" -a -v $*
+}
+
+ToolExists go
+ToolExists date
+ToolExists chmod
+ToolExists chown
+ToolExists upx
+ToolExists fpm
+ToolExists mktemp
+ToolExists xz
+ToolExists gzip
+ToolExists scp
+ToolExists rm
+
 set -e
 
-readonly date="$(date --utc +%Y.%m.%d~%H.%M.%S)"
-readonly tmpdir="$(mktemp -d)"
+rm *.deb *.pkg.tar.?z || true
 
 #go build 2>&1 | grep package | cut -d'"' -f2 | xargs go get -u
-go build -o "${tmpdir}/OffsiteZFSBackup"
-upx --ultra-brute "${tmpdir}/OffsiteZFSBackup"
+### x86_64
+echo "Building x86_64..."
+tmpdir="$(mktemp -d)"
+
+echo "Compiling..."
+compile -o "${tmpdir}/OffsiteZFSBackup"
+echo "Compressing..."
+pack "${tmpdir}/OffsiteZFSBackup"
 chmod +rx ${tmpdir}
 chmod +s "${tmpdir}/OffsiteZFSBackup"
-fpm -n offsite-zfs-backup -v $date -s dir -t pacman -C "${tmpdir}" .=/opt/ozb
-fpm -n offsite-zfs-backup -v $date -s dir -t deb -C "${tmpdir}" .=/opt/ozb
-scp *.deb *.pkg.tar.xz mirror.t4cc0.re:/vault/packages
-rm *.deb *.pkg.tar.xz || true
+echo "Building packages..."
+fpm -n offsite-zfs-backup -v $date -s dir -t pacman -a x86_64 -C "${tmpdir}" .=/opt/ozb
+fpm -n offsite-zfs-backup -v $date -s dir -t deb -a x86_64 -C "${tmpdir}" .=/opt/ozb
+
+### ARM
+echo "Building ARM..."
+tmpdir="$(mktemp -d)"
+
+echo "Compiling..."
+GOARCH=arm GOARM=5 compile -o "${tmpdir}/OffsiteZFSBackup"
+echo "Compressing..."
+pack "${tmpdir}/OffsiteZFSBackup"
+chmod +rx ${tmpdir}
+chmod +s "${tmpdir}/OffsiteZFSBackup"
+echo "Building packages..."
+fpm -n offsite-zfs-backup -v $date -s dir -t deb -a armv6l -C "${tmpdir}" .=/opt/ozb
+fpm -n offsite-zfs-backup -v $date -s dir -t pacman -a armv6l -C "${tmpdir}" .=/opt/ozb
+xz -vd *.pkg.tar.xz
+gzip -v9 *.pkg.tar
+
+### Upload
+echo "Uploading..."
+scp -oProxyJump=jumper *.deb *.pkg.tar.?z blaze.t4cc0.re:/vault/packages
+echo "Cleaning..."
+rm *.deb *.pkg.tar.?z || true
 if [ -n "${tmpdir}" ]; then
   rm -r "${tmpdir}"
 fi
-
-cd -
